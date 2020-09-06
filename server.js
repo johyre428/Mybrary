4542355869c878d54dabacf3f90b9cc9ecaec746
Al-Johyre <al-johyre.abdullah@tooltwist.com.com>
2020-09-06 22:14:20 +0800

    initial authors route

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
+// DB
+const mongoose = require('mongoose')
+const db = mongoose.connection
 
 // Routes
-const indexRoute = require('./routes/index')
+const indexRoutes = require('./routes/index')
+const authorRoutes = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,17 +21,17 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to DB'))
 
-app.use('/', indexRoute)
+app.use('/', indexRoutes)
+app.use('/authors', authorRoutes)
 
 app.listen(process.env.PORT || 3000)
